65adc7016316ac0b4dc2d33f3fc375d657cbdbc3
ShalaevPavel <lohgewt123@yandex.by>
2022-12-15 22:16:53 +0300

    for credit

---
+++ JavaGUI/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import javafx.application.Application;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
